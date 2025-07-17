2310b67656bf7d1374ef63edf27a51b82bb2fde1
Mbccharles <mbccharlesmbumba@gmail.com>
2025-07-17 18:44:52 +0100

    Add files via upload

---
+++ 06/06_exercice05.py
@@ -0,0 +1,10 @@
+n = int(input("Entrez un entier positif :"))
+while n < 0 :
+    print("veuillez entrer un nombre positif.")
+    n = int(input("Entrez un entier positif: "))
+fact = 1
+i = 1
+while i <= n:
+    fact *= i
+    i += 1
+print(f"Factorielle de {n} = {fact} ")
